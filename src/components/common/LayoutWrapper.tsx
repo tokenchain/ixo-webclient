f6b1637c41bd7ea21299c79db28eebe30b583f48
Cedric Franz <cedric.franz@gmail.com>
2018-09-19 14:35:53 +0200

    Added new spinner

--- src/components/common/LayoutWrapper.tsx
+++ src/components/common/LayoutWrapper.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const Container = styled.div`
 	padding:40px;
-	background: ${props => props.theme.bg.blue};
+	background: ${props => props.theme.bg.lightGrey}};
 	color: white;
 `;
 export const LayoutWrapper: React.SFC<{}> = ({children}) => {
--- src/components/common/Spinner.tsx
+++ src/components/common/Spinner.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
+const sdgLogo = require('../../assets/images/SDGLogo.svg');
+
 const Container = styled.div`
     display:flex;
     justify-content:center;
 	align-items:center;
 	flex-direction: column;
-	background-color: ${props => props.theme.bg.blue};
+	background-color: ${props => props.theme.bg.lightGrey};
 	flex:1 1 auto;
 	p {
-		color: ${props => props.theme.ixoBlue}
+		color: #282828
 	}
 `;
 
@@ -19,37 +21,42 @@ const LoaderContainer = styled.div`
 	align-items: center;
 `;
 
-const LoaderWrapper = styled.div`
-	background: ${props => props.theme.ixoBlue};
-	padding: 0;
-	border-radius: 50%;
-	animation: pulse 1.5s infinite ease;
-		
-	@keyframes pulse {
-		0%   { padding: 0; background: rgba(0,210,255,1);}
-		100% { padding: 20px; background: rgba(0,34,51,1);}
+const SpinningImage = styled.div`
+	background-color: ${props => props.theme.bg.lightGrey};
+	animation-name: spinner;
+	animation-duration: 5.5s;
+	animation-iteration-count: infinite;
+	transform-origin: 50% 50%;
+	display: inline-block;
+
+	@keyframes spinner {
+	0% {
+			transform: rotate(0deg);
+	}
+	100% {
+			transform: rotate(360deg);
 	}
 `;
 
-const LoaderInner = styled.div`
+// const LoaderInner = styled.div`
 
-	border-radius: 50%;
-	padding: 5px;
-	background: ${props => props.theme.ixoBlue};
+// 	border-radius: 50%;
+// 	padding: 5px;
+// 	background: ${props => props.theme.ixoBlue};
 
-	i {
-		font-size: 30px;
-		display:block;
-		width: 29px;
-		height: 29px;
-		padding: 0;
-		background: ${props => props.theme.bg.blue};
-		border-radius: 50%;
-	}
-	.icon-ixo-x:before {
-		color: ${props => props.theme.ixoBlue};
-	}
-`;
+// 	i {
+// 		font-size: 30px;
+// 		display:block;
+// 		width: 29px;
+// 		height: 29px;
+// 		padding: 0;
+// 		background: ${props => props.theme.bg.blue};
+// 		border-radius: 50%;
+// 	}
+// 	.icon-ixo-x:before {
+// 		color: ${props => props.theme.ixoBlue};
+// 	}
+// `;
 export interface Props {
 	info: string;
 }
@@ -58,11 +65,9 @@ export const Spinner: React.SFC<Props> = ({info}) => {
 	return (
 		<Container>
 			<LoaderContainer>
-				<LoaderWrapper>
-					<LoaderInner>
-					<i className="icon-ixo-x" />
-					</LoaderInner>
-				</LoaderWrapper>
+				<SpinningImage>
+					<img src={sdgLogo} width="50px" height="50px"/>
+				</SpinningImage>
 			</LoaderContainer>
 			<p>{info}</p>
 		</Container>
