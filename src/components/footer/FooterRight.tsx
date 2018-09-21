ca91ff232ad96f12403a296574189d3646e6ee3e
Cedric Franz <cedric.franz@gmail.com>
2018-09-21 14:13:40 +0200

    Changed footer and added twitter

--- src/components/footer/FooterRight.tsx
+++ src/components/footer/FooterRight.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { deviceWidth } from '../../lib/commonData';
 
 const Main = styled.div`
-	margin-top: 15px;
+	margin-top: 20px;
 	display: flex;
     flex-direction: column;
     justify-content: space-between;
@@ -17,45 +17,47 @@ const Main = styled.div`
 	}
 `;
 
-// const SocialIcon = styled.a`
-// 	padding: 10px 10px 0px;
+const SocialIcon = styled.a`
+	padding: 10px 10px 0px;
+	font-size: 20px;
 
-// 	:before {
-// 		color: #FFF;
-// 	}
+	:before {
+		color: #FFF;
+	}
 	
-// 	&:hover:before {
-// 		text-decoration: none;
-// 		color: ${props => props.theme.ixoBlue};
-// 	}
+	&:hover:before {
+		text-decoration: none;
+		color: ${props => props.theme.ixoBlue};
+	}
 	
-// 	&&:hover {
-// 		text-decoration: none;
-// 	}
-// `;
+	&&:hover {
+		text-decoration: none;
+	}
+`;
 
-// const SocialIconContainer = styled.div`
-// 	margin-top: 20px;
-// 	margin-bottom: 10px;
+const SocialIconContainer = styled.div`
+	margin-top: 10px;
+	margin-bottom: 10px;
 
-// 	@media (min-width:${deviceWidth.tablet}px) {
-// 		margin: 0;
-// 		padding-right: 60px;
-// 	}
-// `;
+	@media (min-width:${deviceWidth.tablet}px) {
+		margin: 0;
+		padding-right: 60px;
+	}
+`;
+
 export const FooterRight: React.SFC<any> = () => {
 	return (
 		<Main className="col-md-4">
 			<div className="row">
 				{/* In partnership with <a href={getIxoWorldRoute('/')}><IXOLogo alt="ixo Logo" src={ixoLogo}/></a> for the Future of Humanity */}
-				{/* <SocialIconContainer className="col-md-12">
-					<SocialIcon href="https://twitter.com/ixoworld?lang=en" target="_blank" className="icon-twitter" />
-					<SocialIcon href="https://www.facebook.com/ixoworld/" target="_blank" className="icon-facebook" />
+				<SocialIconContainer className="col-md-12">
+					<SocialIcon href="https://twitter.com/sdgfutures" target="_blank" className="icon-twitter" />
+					{/* <SocialIcon href="https://www.facebook.com/ixoworld/" target="_blank" className="icon-facebook" />
 					<SocialIcon href="https://www.linkedin.com/company/25001970/admin/updates/" target="_blank" className="icon-linkedin" />
 					<SocialIcon href="https://github.com/ixofoundation" target="_blank" className="icon-github" />
 					<SocialIcon href="https://medium.com/ixo-blog" target="_blank" className="icon-medium" />
-					<SocialIcon href="https://t.me/joinchat/Ejz5exAfFUzcBMRlaYLecQ" target="_blank" className="icon-telegram" />
-				</SocialIconContainer> */}
+					<SocialIcon href="https://t.me/joinchat/Ejz5exAfFUzcBMRlaYLecQ" target="_blank" className="icon-telegram" /> */}
+				</SocialIconContainer>
 			</div>
 		</Main>
 	);
