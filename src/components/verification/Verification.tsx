d54812ac612d28ac621a6e77b8a258802fd42839
donnierisk <donnierisk@gmail.com>
2018-09-27 18:03:24 +0200

    basic verification upgraded to semi-intermediate

--- src/components/verification/Verification.tsx
+++ src/components/verification/Verification.tsx
@@ -125,12 +125,10 @@ export class Verification extends React.Component<ParentProps, State> {
 
 	componentDidMount() {
 		this.checkState();
-		if (this.state.hasKeySafe === false || this.state.isDidLedgered === false || !!this.state.didDoc === false) {
-			setTimeout(() => this.checkState(), 1000);
-		}
+		setTimeout(() => this.checkState(), 2000);
 	}
 
-	checkState() {
+	checkState = () => {
 		// If the user has a keysafe and but the hasKeySafe not set then set state
 		if (this.props.keysafe && !this.state.hasKeySafe) {
 			this.props.keysafe.getDidDoc((error, response) => {
@@ -150,20 +148,11 @@ export class Verification extends React.Component<ParentProps, State> {
 			this.props.ixo.user.getDidDoc(this.state.didDoc.did).then((didResponse: any) => {
 				if (didResponse.did) {
 						this.setState({isDidLedgered: true, didDoc: didResponse});
-				} else {
-					// Did not ledgered
-					this.ledgerDid();
 				}
-			})
-			.catch((err) => {
-					// Did not ledgered
-					this.ledgerDid();
 			});
-		} else if (this.props.ixo && this.state.didDoc) {
-			console.log('test');
+		} else if (this.props.ixo && this.state.didDoc && !this.state.isDidLedgered) {
 			this.setState({isDidLedgered: true});
 		}
-		
 	}
 
 	getIcon = (condition) => {
@@ -211,37 +200,17 @@ export class Verification extends React.Component<ParentProps, State> {
 	}
 
 	getLedgeredText = () => {
+		
 		return (
 			<CheckItem>
-				{this.getIcon(this.state.isDidLedgered)} <strong><a onClick={this.ledgerDid} target="_blank">Register your identity</a> on the ixo blockchain</strong>
+				{this.getIcon(this.state.isDidLedgered)} <strong>{this.state.isDidLedgered === true ? 'Register your identity' : <a onClick={this.ledgerDid} target="_blank">Register your identity</a>} on the ixo blockchain</strong>
 				<span>By registering, you are cryptographically verifying your identity to ensure all your interactions within SDG Futures are secure.</span>
 			</CheckItem>
 		);
 	}
 
-		// So has a client side didDoc, so lets check if it is ledgered
-		// if (this.props.ixo && this.state.didDoc && !this.state.isDidLedgered) {
-		// 	this.props.ixo.user.getDidDoc(this.state.didDoc.did).then((didResponse: any) => {
-		// 		if (didResponse.did) {
-		// 			if (didResponse.credentials.length === 0) {
-		// 				// Has no KYC Credential (Should look at the detail here, but right now we only have one type of credential)
-		// 				this.setState({isDidLedgered: true, didDoc: didResponse, hasKYC: false});
-		// 			} else {
-		// 				this.setState({isDidLedgered: true, didDoc: didResponse, hasKYC: true});
-		// 			}
-		// 		} else {
-		// 			// Did not ledgered
-		// 			this.ledgerDid();
-		// 		}
-		// 	})
-		// 	.catch((err) => {
-		// 			// Did not ledgered
-		// 			this.ledgerDid();
-					
-		// 	});
-		// }
-
 	ledgerDid = () => {
+		console.log(this.state.didDoc);
 		if (this.props.ixo && this.state.didDoc && !this.state.isDidLedgered && !this.busyLedgering) {
 			let payload = {didDoc: this.state.didDoc};
 			this.busyLedgering = true;
@@ -250,9 +219,8 @@ export class Verification extends React.Component<ParentProps, State> {
 					this.props.ixo.user.registerUserDid(payload, signature).then((response: any) => {
 						if (response.code === 0) {
 							successToast('Did document was ledgered successfully');
-							console.log('ledgered successfully');
+							this.setState({isDidLedgered: true});
 						} else {
-							console.log('unable to ledger at this time');
 							errorToast('Unable to ledger did at this time');
 						}
 						// Delay the update here to allow Explorer to sync
